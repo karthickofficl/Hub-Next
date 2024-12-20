2aa6bb04c1d7076c8325557673133bceac785035
karthickofficl <karthikraja@akkenna.com>
2024-12-20 23:19:45 +0530

    fix: build

--- src/lib/api/deliveryUserApi.ts
+++ src/lib/api/deliveryUserApi.ts
@@ -1,22 +1,24 @@
 import axios from "axios";
-const token = localStorage.getItem("token");
 
-console.log("token", token);
 // Orders API Handler
 export const getUsers = async () => {
   try {
+    // Access localStorage within the function to ensure it's client-side
+    const token = localStorage.getItem("token");
+    console.log("token", token);
+
     const response = await axios.get(
       `${process.env.NEXT_PUBLIC_API_BASE_URL}/delivery/user/getUsers`,
       {
         headers: {
           "Content-Type": "application/json",
-          "Authorization": `Bearer ${token}`
+          Authorization: `Bearer ${token}`,
         },
       }
     );
-    return response.data; // Ensure this returns an array of orders
+    return response.data; // Ensure this returns an array of users
   } catch (error) {
-    console.error("Error fetching orders:", error);
+    console.error("Error fetching users:", error);
     throw error; // Rethrow for further handling
   }
 };
--- src/lib/api/ordersAPI.ts
+++ src/lib/api/ordersAPI.ts
@@ -1,16 +1,17 @@
 import axios from "axios";
-const token = localStorage.getItem("token");
 
-console.log("token", token);
 // Orders API Handler
 export const getOrders = async () => {
   try {
+    // Access localStorage within the function to ensure it's client-side
+    const token = localStorage.getItem("token");
+    console.log("token", token);
     const response = await axios.get(
       `${process.env.NEXT_PUBLIC_API_BASE_URL}/delivery/order/getOrders`,
       {
         headers: {
           "Content-Type": "application/json",
-          "Authorization": `Bearer ${token}`
+          Authorization: `Bearer ${token}`,
         },
       }
     );
