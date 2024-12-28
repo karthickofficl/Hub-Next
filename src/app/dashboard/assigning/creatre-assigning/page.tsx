6b9e4f3261dbeaf17a89d273dfc96bf666788325
karthickofficl <karthikraja@akkenna.com>
2024-12-28 18:40:39 +0530

    API URL changed

--- src/app/dashboard/assigning/creatre-assigning/page.tsx
+++ src/app/dashboard/assigning/creatre-assigning/page.tsx
@@ -1,77 +1,260 @@
-import React from 'react'
+import React from "react";
 
 const CreateAssigning = () => {
   return (
     <div className="border-b border-gray-900/10 pb-12">
-      <h2 className="text-base/7 font-semibold text-gray-900 font-[family-name:var(--interSemiBold)]">Personal Information</h2>
-      <p className="mt-1 text-sm/6 text-gray-600 font-[family-name:var(--interRegular)]">Use a permanent address where you can receive mail.</p>
-
+      <h2 className="text-base/7 font-semibold text-gray-900 font-[family-name:var(--interSemiBold)]">
+        Assigning Order to Delivery Boys
+      </h2>
+      <p className="mt-1 text-sm/6 text-gray-600 font-[family-name:var(--interRegular)]">
+        Use a permanent address where you can receive order.
+      </p>
       <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
         <div className="sm:col-span-3">
-          <label htmlFor="first-name" className="block text-sm/6 font-medium text-gray-900 font-[family-name:var(--interRegular)]">First name</label>
+          <label
+            htmlFor="country"
+            className="block text-sm/6 font-medium text-gray-900"
+          >
+            Today Orders
+          </label>
+          <div className="mt-2 grid grid-cols-1">
+            <select
+              id="country"
+              name="country"
+              autoComplete="country-name"
+              className="col-start-1 row-start-1 w-full appearance-none rounded-md bg-white py-1.5 pl-3 pr-8 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+            >
+              <option>Milk - Fazil, thudiyalure-543456</option>
+              <option>Ghee - Vino, chennai-674545</option>
+              <option>Mexico</option>
+            </select>
+            <svg
+              className="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-500 sm:size-4"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              aria-hidden="true"
+              data-slot="icon"
+            >
+              <path
+                fillRule="evenodd"
+                d="M4.22 6.22a.75.75 0 0 1 1.06 0L8 8.94l2.72-2.72a.75.75 0 1 1 1.06 1.06l-3.25 3.25a.75.75 0 0 1-1.06 0L4.22 7.28a.75.75 0 0 1 0-1.06Z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </div>
+        </div>
+
+        {/* Delivery Boy */}
+
+        <div className="sm:col-span-3">
+          <label
+            htmlFor="country"
+            className="block text-sm/6 font-medium text-gray-900"
+          >
+            Delivery partner
+          </label>
+          <div className="mt-2 grid grid-cols-1">
+            <select
+              id="country"
+              name="country"
+              autoComplete="country-name"
+              className="col-start-1 row-start-1 w-full appearance-none rounded-md bg-white py-1.5 pl-3 pr-8 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+            >
+              <option>Ram kumar - chennai</option>
+              <option>Anand - covai</option>
+              <option>Mexico</option>
+            </select>
+            <svg
+              className="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-500 sm:size-4"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              aria-hidden="true"
+              data-slot="icon"
+            >
+              <path
+                fillRule="evenodd"
+                d="M4.22 6.22a.75.75 0 0 1 1.06 0L8 8.94l2.72-2.72a.75.75 0 1 1 1.06 1.06l-3.25 3.25a.75.75 0 0 1-1.06 0L4.22 7.28a.75.75 0 0 1 0-1.06Z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </div>
+        </div>
+
+        {/* Button */}
+
+        <div>
+          <button
+            type="submit"
+            className="font-[family-name:var(--interSemiBold)] flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+           Assign Order
+          </button>
+        </div>
+      </div>
+
+      {/* <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+        <div className="sm:col-span-3">
+          <label
+            htmlFor="first-name"
+            className="block text-sm/6 font-medium text-gray-900 font-[family-name:var(--interRegular)]"
+          >
+            First name
+          </label>
           <div className="mt-2">
-            <input type="text" name="first-name" id="first-name" autoComplete="given-name" className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+            <input
+              type="text"
+              name="first-name"
+              id="first-name"
+              autoComplete="given-name"
+              className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+            />
           </div>
         </div>
 
         <div className="sm:col-span-3">
-          <label htmlFor="last-name" className="block text-sm/6 font-medium text-gray-900 font-[family-name:var(--interRegular)]">Last name</label>
+          <label
+            htmlFor="last-name"
+            className="block text-sm/6 font-medium text-gray-900 font-[family-name:var(--interRegular)]"
+          >
+            Last name
+          </label>
           <div className="mt-2">
-            <input type="text" name="last-name" id="last-name" autoComplete="family-name" className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+            <input
+              type="text"
+              name="last-name"
+              id="last-name"
+              autoComplete="family-name"
+              className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+            />
           </div>
         </div>
 
         <div className="sm:col-span-4">
-          <label htmlFor="email" className="block text-sm/6 font-medium text-gray-900">Email address</label>
+          <label
+            htmlFor="email"
+            className="block text-sm/6 font-medium text-gray-900"
+          >
+            Email address
+          </label>
           <div className="mt-2">
-            <input id="email" name="email" type="email" autoComplete="email" className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+            <input
+              id="email"
+              name="email"
+              type="email"
+              autoComplete="email"
+              className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+            />
           </div>
         </div>
 
         <div className="sm:col-span-3">
-          <label htmlFor="country" className="block text-sm/6 font-medium text-gray-900">Country</label>
+          <label
+            htmlFor="country"
+            className="block text-sm/6 font-medium text-gray-900"
+          >
+            Country
+          </label>
           <div className="mt-2 grid grid-cols-1">
-            <select id="country" name="country" autoComplete="country-name" className="col-start-1 row-start-1 w-full appearance-none rounded-md bg-white py-1.5 pl-3 pr-8 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
+            <select
+              id="country"
+              name="country"
+              autoComplete="country-name"
+              className="col-start-1 row-start-1 w-full appearance-none rounded-md bg-white py-1.5 pl-3 pr-8 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+            >
               <option>United States</option>
               <option>Canada</option>
               <option>Mexico</option>
             </select>
-            <svg className="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-500 sm:size-4" viewBox="0 0 16 16" fill="currentColor" aria-hidden="true" data-slot="icon">
-              <path fillRule="evenodd" d="M4.22 6.22a.75.75 0 0 1 1.06 0L8 8.94l2.72-2.72a.75.75 0 1 1 1.06 1.06l-3.25 3.25a.75.75 0 0 1-1.06 0L4.22 7.28a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
+            <svg
+              className="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-500 sm:size-4"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              aria-hidden="true"
+              data-slot="icon"
+            >
+              <path
+                fillRule="evenodd"
+                d="M4.22 6.22a.75.75 0 0 1 1.06 0L8 8.94l2.72-2.72a.75.75 0 1 1 1.06 1.06l-3.25 3.25a.75.75 0 0 1-1.06 0L4.22 7.28a.75.75 0 0 1 0-1.06Z"
+                clipRule="evenodd"
+              />
             </svg>
           </div>
         </div>
 
         <div className="col-span-full">
-          <label htmlFor="street-address" className="block text-sm/6 font-medium text-gray-900">Street address</label>
+          <label
+            htmlFor="street-address"
+            className="block text-sm/6 font-medium text-gray-900"
+          >
+            Street address
+          </label>
           <div className="mt-2">
-            <input type="text" name="street-address" id="street-address" autoComplete="street-address" className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+            <input
+              type="text"
+              name="street-address"
+              id="street-address"
+              autoComplete="street-address"
+              className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+            />
           </div>
         </div>
 
         <div className="sm:col-span-2 sm:col-start-1">
-          <label htmlFor="city" className="block text-sm/6 font-medium text-gray-900">City</label>
+          <label
+            htmlFor="city"
+            className="block text-sm/6 font-medium text-gray-900"
+          >
+            City
+          </label>
           <div className="mt-2">
-            <input type="text" name="city" id="city" autoComplete="address-level2" className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+            <input
+              type="text"
+              name="city"
+              id="city"
+              autoComplete="address-level2"
+              className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+            />
           </div>
         </div>
 
         <div className="sm:col-span-2">
-          <label htmlFor="region" className="block text-sm/6 font-medium text-gray-900">State / Province</label>
+          <label
+            htmlFor="region"
+            className="block text-sm/6 font-medium text-gray-900"
+          >
+            State / Province
+          </label>
           <div className="mt-2">
-            <input type="text" name="region" id="region" autoComplete="address-level1" className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+            <input
+              type="text"
+              name="region"
+              id="region"
+              autoComplete="address-level1"
+              className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+            />
           </div>
         </div>
 
         <div className="sm:col-span-2">
-          <label htmlFor="postal-code" className="block text-sm/6 font-medium text-gray-900">ZIP / Postal code</label>
+          <label
+            htmlFor="postal-code"
+            className="block text-sm/6 font-medium text-gray-900"
+          >
+            ZIP / Postal code
+          </label>
           <div className="mt-2">
-            <input type="text" name="postal-code" id="postal-code" autoComplete="postal-code" className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+            <input
+              type="text"
+              name="postal-code"
+              id="postal-code"
+              autoComplete="postal-code"
+              className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+            />
           </div>
         </div>
-      </div>
+      </div> */}
     </div>
-  )
-}
+  );
+};
 
 export default CreateAssigning;
