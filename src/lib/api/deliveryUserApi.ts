b02c5fd2711b5a7015e44fadee15548de0df3161
karthickofficl <karthikraja@akkenna.com>
2025-01-08 01:53:41 +0530

    new: DeliveryUsersList

--- src/lib/api/deliveryUserApi.ts
+++ src/lib/api/deliveryUserApi.ts
@@ -1,14 +1,13 @@
 import axios from "axios";
-
 // Orders API Handler
-export const getUsers = async () => {
+export const getUsers = async (hubuserId: string, username: string, email: string, page: string, limit: string) => {
   try {
     // Access localStorage within the function to ensure it's client-side
     const token = localStorage.getItem("token");
     console.log("token", token);
 
     const response = await axios.get(
-      `${process.env.NEXT_PUBLIC_API_BASE_URL}/delivery/user/getUsers`,
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}/delivery/user/getAllDeliveryUsersHub?hubuserId=${hubuserId}&username=${username}&email=${email}&page=${page}&limit=${limit}`,
       {
         headers: {
           "Content-Type": "application/json",
