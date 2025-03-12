05b5d711252a2251a695c644b8ee40a243d0609a
karthickofficl <karthickrajadev4@gmail.com>
2025-03-13 00:50:00 +0530

    hub issues fine - 13-03-25

--- src/app/dashboard/payment-request/page.tsx
+++ src/app/dashboard/payment-request/page.tsx
@@ -248,25 +248,25 @@ const PaymentRequest = () => {
           <div className="w-full">
             <button
               type="button"
+              onClick={() => handleOpenPopup()}
               //   onClick={handleRefresh}
               className="font-[family-name:var(--interRegular)] bg-amber-900 text-white rounded flex items-center px-2 py-1"
             >
               <svg
-                onClick={() => handleOpenPopup()}
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
                 strokeWidth={1.5}
                 stroke="currentColor"
-                className="size-6 pr-2"
+                className="size-6"
               >
                 <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
-                  d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
+                  d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
                 />
               </svg>
-              Add Payment Request
+              Add Payment
             </button>
           </div>
         </div>
@@ -445,7 +445,7 @@ const PaymentRequest = () => {
           </div>
         </div>
       )}
-      ;{/* Popup */}
+      {/* Popup */}
       {isPopupOpen && (
         <div className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50">
           <div className="bg-white p-6 rounded-md shadow-lg w-full max-w-xl">
